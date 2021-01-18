987c26c0702c7e6115ecf4c5e99ca48e65acc289
Rene Montoya <rmontoya.we@gmail.com>
2021-01-17 18:52:23 -0800

    finishing min to front

---
+++ min2front.js
@@ -0,0 +1,17 @@
+function mintoFront(arr) {
+    var holder;
+    var min = arr[0];
+    // first need to find the min in the whole array
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+            var index = i;
+        }
+    }
+    console.log('min is', min);
+    holder = arr[0];
+    arr[0] = min;
+    arr[index] = holder;
+    console.log('see', arr);
+}
+mintoFront([2, 3, 1, 4, 5, -1]);
